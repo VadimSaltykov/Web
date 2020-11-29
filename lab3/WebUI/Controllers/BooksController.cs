24d3672588203acd5785f4961864577851b795ae
Vadim Saltykov <saltykov-vadik@mail.ru>
2020-11-29 14:32:53 +0300

    add lab3

---
+++ lab3/WebUI/Controllers/BooksController.cs
@@ -0,0 +1,55 @@
+﻿using Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebUI.Models;
+
+namespace WebUI.Controllers
+{
+    public class BooksController : Controller
+    {
+        private IBookRepository repository;
+        public int pageSize = 4;
+
+        public BooksController(IBookRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult List(string genre, int page = 1)
+        {
+            BooksListViewModel model = new BooksListViewModel
+            {
+                Books = repository.Books
+                .Where(b => genre == null || b.Genre == genre)
+                .OrderBy(book => book.BookId)
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = pageSize,
+                    TotalItems = genre == null ?
+                        repository.Books.Count() :
+                        repository.Books.Where(book => book.Genre == genre).Count()
+                },
+                CurrentGenre = genre
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult BookSearch(string name)
+        {
+            var allbooks = repository.Books.Where(a => a.Author.Contains(name)).ToList();
+            if (allbooks.Count <= 0)
+            {
+                return HttpNotFound();
+            }
+            return PartialView(allbooks);
+        }
+    }
+}
